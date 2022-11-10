bdbe98de0b6357991814f4aec3ce1a91ecba6c1a
Gabriel <pytajnick.pl@gmail.com>
2022-11-10 19:02:23 +0100

    1 push xd ann

---
+++ 3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(string napis)
+{
+    int sum=0;
+    cout<<sizeof(napis)<<"  "<<sizeof(char);
+    for(size_t i = 0; i < napis.length; i++)
+    {
+        cout<<" "<<i<<endl;
+        if (0<napis[i]-48 && napis[i]-48 <10)
+        {
+            
+            sum+=napis[i]-48;
+            cout<<sum<<endl;
+        }
+
+    }
+    return sum;
+}
+
+int main()
+{
+  string napis= "Ala kupila 3 kg pomaranczy, 12 jablek i 2 litry soku";
+  int h=fun(napis);
+  cout<<h;
+  return 0;
+}
