65837b431991b961125fd36abe1ed31845ac1ec7
Gabriel <pytajnick.pl@gmail.com>
2022-11-19 15:06:40 +0100

    dziala

--- p1.cpp
+++ p1.cpp
@@ -16,9 +16,21 @@ class Student
     string gkierunek()  {return kierunek;}
     string grok()  {return rok;}
 
+    void simie(string s)
+    {
+        imie=s;
+    }
     void snazwisko(string s)
     {
         nazwisko=s;
+    }
+    void skierunek(string s)
+    {
+        kierunek=s;
+    }
+    void srok(string s)
+    {
+        rok=s;
     }
 
 
@@ -30,6 +42,7 @@ class Student
         rok=r;
 
     }
+    Student(){}
 
 
     
@@ -59,10 +72,10 @@ int Grupa::counter=0;
 
 void Grupa::wypisz()
 {
-    cout<< numer_grupy <<" "<< limit_miejsc;
+    cout<< " numer grupy " << numer_grupy <<" limit miejsc "<< limit_miejsc << " "<< endl;
     for (size_t i = 0; i < limit_miejsc; i++)
     {
-        cout<< tab[i].gimie() << tab[i].gnazwisko() << tab[i].gkierunek() << tab[i].grok()<<endl;
+        cout<< tab[i].gimie() <<" "<< tab[i].gnazwisko() <<" "<< tab[i].gkierunek() <<" "<< tab[i].grok()<<endl;
     }
     
 
@@ -70,29 +83,89 @@ void Grupa::wypisz()
 
 void Grupa::dodajStudenta( Student Stu)
 {
-    tab[counter]=Stu;
-    counter++;
-}
+    if (counter>9)
+    {
+        cout<<" "<<" nie mozna dodac "<<endl;
+    }
+    else
+    {
+        tab[counter]=Stu;
+        counter++;
+    }
+
+    }
+
 
 void Grupa::zmienNazwisko(string nazstare, string naznowe)
 {
     int i;
-    for ( i = 0; i < counter; i++)
+    for ( i = 0; i < counter+1; i++)
     {
         if (tab[i].gnazwisko()==nazstare)
             break;
     }
-
+    if(i==counter+1)
+        cout<<"nie ma takiego nazwiska"<<" ";
+    else
     tab[i].snazwisko(naznowe);
 }
 
 
 int main()
 {
-    Student stu("a", "b", "c", "d");
-    //Grupa gru(1);
-    //gru.dodajStudenta(stu);
-    //gru.wypisz();
+    Student stu("Jan", "Kowalski", "informatyka", "1");
+    Student stu2("Jan", "Nowak", "informatyka", "2");
+    Student stu3("Jan", "Kowal", "informatyka", "3");
+
+    Grupa gru(1);
+    gru.dodajStudenta(stu);
+    gru.dodajStudenta(stu2);
+    gru.dodajStudenta(stu3);
+    gru.wypisz();
+    gru.zmienNazwisko("Kowal", "Polak");
+    gru.wypisz();
+
+    Student *tab=new Student[2];
+    tab[0].simie("Piotr");
+    tab[0].snazwisko("Skala");
+    tab[0].skierunek("fizyka");
+    tab[0].srok("2");
+    tab[1].simie("Zbigniew");
+    tab[1].snazwisko("Matczak");
+    tab[1].skierunek("fizyka");
+    tab[1].srok("3");
+
+    cout<<endl;
+
+    gru.wypisz();
+    for (size_t i = 0; i < 2; i++)
+    {
+        cout<< tab[i].gimie() <<" "<< tab[i].gnazwisko() <<" "<< tab[i].gkierunek() <<" "<< tab[i].grok()<<endl;
+    }
+
+
+    gru.dodajStudenta(tab[0]);
+    gru.dodajStudenta(tab[1]);
+
+
+    Student stu4("Jan1", "Kowalski1", "informatyka", "1");
+    Student stu5("Jan2", "Nowak2", "informatyka", "2");
+    Student stu6("Jan3", "Kowal3", "informatyka", "3");
+    Student stu7("Jan4", "Kowalski4", "informatyka", "1");
+    Student stu8("Jan5", "Nowak5", "informatyka", "2");
+    Student stu9("Jan6", "Kowal6", "informatyka", "3");
+
+
+    gru.dodajStudenta(stu4);
+    gru.dodajStudenta(stu5);
+    gru.dodajStudenta(stu6);
+    gru.dodajStudenta(stu7);
+    gru.dodajStudenta(stu8);
+    gru.dodajStudenta(stu9);
+    
+    gru.wypisz();
+
+
     
     return 0;
 }
