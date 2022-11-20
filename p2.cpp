3ec5e7f9336abea0260bcb25b27e61e15488231d
Gabriel <pytajnick.pl@gmail.com>
2022-11-20 23:07:26 +0100

    xd

--- p2.cpp
+++ p2.cpp
@@ -11,4 +11,72 @@ class Pasazer
 
     public:
     
+    Pasazer();
+    Pasazer(string, string, string);
+    Pasazer(const Pasazer &);
+
+    int glicznik(){return licznik;}
+
+
 };
+Pasazer::Pasazer()
+{
+    licznik++;
+}
+Pasazer::Pasazer(string a, string b, string c)
+{
+    imie=a;
+    nazwisko=b;
+    rok_urodzenia=c;
+    licznik++;
+}
+Pasazer::Pasazer(const Pasazer & wzor)
+{
+    imie=wzor.imie;
+    nazwisko=wzor.nazwisko;
+    rok_urodzenia=wzor.rok_urodzenia;
+    licznik++;
+    
+}
+
+class Autokar
+{
+    friend class Pasazer;
+    int miejsce_docelowe;
+    int vmax;
+    static const int liczba_miejsc=10;
+    Pasazer* tab[liczba_miejsc];
+
+    public:
+
+    Autokar(const Autokar &);
+    void wypisz();
+};
+
+Autokar::Autokar(const Autokar & wzor)
+{
+    miejsce_docelowe = wzor.miejsce_docelowe;
+    vmax = wzor.vmax;
+    Pasazer * tab= tab;
+    
+}
+
+
+void Autokar::wypisz()
+{
+    for(int i=0; i<3/*Pasazer::glicznik()*/; i++)
+    {
+        cout<<tab[i]->imie
+    }    
+}
+
+
+
+int main()
+{
+    Pasazer pas1;
+    Pasazer pas2("Tadek", "Wegrzyn", "2000");
+    Pasazer pas3(pas2);
+
+    return 0;
+}
